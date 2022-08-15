ba3970ecc0283007e147024c8be8e4c79cb2e593
Marine BD <marine.bap-deletang@aum.bio>
2022-08-15 20:33:47 +0200

    debug basic auth

--- epicevent/settings.py
+++ epicevent/settings.py
@@ -145,7 +145,6 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 100,
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
     "DEFAULT_PERMISSION_CLASSES": [
@@ -154,7 +153,6 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend", 
-        # "rest_framework_filters.backends.DjangoFilterBackend"
         ]
 }
 
